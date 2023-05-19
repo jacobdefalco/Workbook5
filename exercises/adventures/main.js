aff14dc6c8b94af22174e1cbbeb2bde511bffd34
Jacob DeFalco <jake.defalco97@gmail.com>
2023-05-19 15:40:24 -0400

    final commit

--- exercises/adventures/main.js
+++ exercises/adventures/main.js
@@ -18,8 +18,8 @@ const matchingActivitieseTblBody = document.querySelector(
 function loadChoices() {
   categories.forEach((category) => {
     //loops category array
-    let categorySelection = new Option(category.name); //assigns categories to their own "category"
-    selection.appendChild(categorySelection); //displays categories as options
+    let categoryOptions = new Option(category.name); //assigns categories to their own "category"
+    selection.appendChild(categoryOptions); //displays categories as options
   });
   loadButton.disabled = true;
   loadButton.innerText = "Load Categories(Disabled)";
@@ -143,13 +143,16 @@ function buildActivityRow(tbody, activity) {
   cell1.innerText = activity.name;
 
   let cell2 = row.insertCell(1);
-  cell2.innerText = "$" + activity.price.toFixed(2);
+  cell2.innerText = activity.description;
 
   let cell3 = row.insertCell(2);
-  cell3.innerText = activity.location;
+  cell3.innerText = "$" + activity.price.toFixed(2);
 
   let cell4 = row.insertCell(3);
-  cell4.innerText = activity.id;
+  cell4.innerText = activity.location;
+
+  let cell5 = row.insertCell(4);
+  cell5.innerText = activity.id;
 }
 
 function filterActivities() {
@@ -157,12 +160,14 @@ function filterActivities() {
 }
 
 function displayMatchingActivitiesTable(matchingActivity) {
+  matchingActivitieseTblBody.innerHTML = "";
   for (let i = 0; i < matchingActivity.length; i++) {
     buildActivityRow(matchingActivitieseTblBody, matchingActivity[i]);
   }
 }
 
 function filterAndDisplay() {
+  matchingActivitieseTblBody.innerHTML = "";
   let filteredActivities = filterActivities();
   displayMatchingActivitiesTable(filteredActivities);
 }
